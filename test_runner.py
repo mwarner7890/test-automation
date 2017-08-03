11319ca6cc2488c8f543a907f1d145ed0b4b1ef9
Michael <mwarner@bullitt-group.com>
2017-08-03 17:06:23 +0100

    minor rearrangement

--- test_runner.py
+++ test_runner.py
@@ -6,16 +6,18 @@ from pyadb import ADB
 class TestRunner(unittest.TestCase):
     def setUp(self):
         self.adb = ADB()
-        self.adb.start_server()
         self.adb.set_adb_path('/usr/bin/adb')
+        self.adb.start_server()
 
         print 'DEBUG: waiting for device'
-        print adb_test_functions.\
+        # TODO: Probably revise adb_test_functions approach...
+        print adb_test_functions. \
             wait_for_device(self.adb)
 
     def test_unlock_screen(self):
         print 'unlocking screen...'
         adb_test_functions.unlock_screen_with_pin(self.adb)
+        adb_test_functions.input_key_event(self.adb, 'KEYCODE_POWER')
 
 
 if __name__ == '__main__':
