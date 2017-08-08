fea10e7ce5b4dc5ebc72d44df41f647003b6dc51
Michael Warner <mwarner@bullitt-group.com>
2017-08-08 16:45:28 +0100

    Compiled .exe (on Windows)

--- src/setup.py
+++ src/setup.py
@@ -10,6 +10,8 @@ executables = [
     Executable('test_runner.py', base=base, targetName = 'test_automation_runner.exe')
 ]
 
+includefiles = ['../lib/adb.py']
+
 setup(name='TestAutomation',
       version = '1',
       description = 'Automates tests',
