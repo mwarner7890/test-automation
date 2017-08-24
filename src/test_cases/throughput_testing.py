7ee2acbaf13fa81788c6e4509ee8a65bee924b11
Michael <mwarner@bullitt-group.com>
2017-08-24 15:59:54 +0100

    Toggle USB tethering

--- src/test_cases/throughput_testing.py
+++ src/test_cases/throughput_testing.py
@@ -1,14 +1,15 @@
 def _toggle_usb_tethering(adb):
     adb.launch_activity('com.android.settings/.TetherSettings')
-    adb.input_tap_abs(210, 360)
+    adb.input_tap(x="29%", y="28%")
 
 
-def _test_throughput(adb):
-    return ftp_file_path
+# def _test_throughput(adb):
+#     return ftp_file_path
 
 
 def test_2g_throughput(adb):
     print('TODO: 2G throughput test!')
+    _toggle_usb_tethering(adb)
 
 
 def test_3g_throughput(adb):
