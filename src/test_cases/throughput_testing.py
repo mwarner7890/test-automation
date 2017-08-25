980245a72b1feb83669053b0845caadb5ac81107
MWarner <mwarner@bullitt-group.com>
2017-08-25 17:15:40 +0100

    General idea to approach

--- src/test_cases/throughput_testing.py
+++ src/test_cases/throughput_testing.py
@@ -1,5 +1,6 @@
 import time
 
+
 def _toggle_usb_tethering(adb):
     adb.launch_activity('com.android.settings/.TetherSettings')
     adb.input_tap(x="29%", y="28%")
@@ -13,6 +14,9 @@ def test_2g_throughput(adb):
     print('TODO: 2G throughput test!')
     _toggle_usb_tethering(adb)
     time.sleep(3)
+    print('FTP downloading stuff happens now...')
+    _toggle_usb_tethering(adb)
+    time.sleep(5)
 
 
 def test_3g_throughput(adb):
