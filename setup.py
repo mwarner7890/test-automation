7767230cb772ac85b1205d91ad0f22e54070d14a
Michael Warner <mwarner@bullitt-group.com>
2017-10-10 16:21:39 +0100

    New build

---
+++ setup.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = [], excludes = [])
+
+base = 'Console'
+
+executables = [
+    Executable('src/test_runner.py', base=base, targetName = 'test_automation_runner.exe')
+]
+
+includefiles = ['lib/adb.py']
+
+setup(name='TestAutomation',
+      version = '1',
+      description = 'Automates tests',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
